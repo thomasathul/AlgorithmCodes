eb54dea925c99a0257e5021380548bfbcf9aad64
Athul Thomas <64976137+thomasathul@users.noreply.github.com>
2021-03-22 11:25:39 +0530

    Update binarysearch.java

--- searching/binarysearch.java
+++ searching/binarysearch.java
@@ -1,13 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.mycompany.searching;
-/**
- *
- * @author Dell
- */
+import java.util.*;
 public class binarysearch {
     public static void main(String args[])
     {
